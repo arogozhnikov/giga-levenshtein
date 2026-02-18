1e0f5323fa61a4a37c7341241e9036eda1ef9eba
Alex Rogozhnikov <iamfullofspam@gmail.com>
2026-02-17 20:16:54 -0800

    bity simd - 8 passed tests

--- src/simd.rs
+++ src/simd.rs
@@ -1,7 +1,7 @@
 use std::array;
-
 use std::simd::cmp::SimdOrd;
 use std::simd::cmp::SimdPartialEq;
+use std::simd::Mask;
 use std::simd::{Select, Simd};
 use std::time::Instant;
 
@@ -22,14 +22,14 @@ fn levenshtein(a: &str, b: &str) -> usize {
 
 fn bitty_levenshtein(a: &[u8], b: &[u8]) -> u8 {
     // myers-style algo
-    let n = b.len();
-    if n == 0 {
+    if b.len() == 0 {
         return a.len() as u8;
     };
     if a.len() == 0 {
         return b.len() as u8;
     }
     // initialize with all ones
+    let n = b.len();
     let mut prev_hp = vec![true; n];
     let mut prev_hn = vec![false; n];
     //
@@ -76,30 +76,73 @@ fn bitty_levenshtein(a: &[u8], b: &[u8]) -> u8 {
 
 type U8<const N: usize> = Simd<u8, N>;
 
-// fn bitty_levenshtein_simd_by_1<const N: usize>(a: &[&[u8]; N * 8], b: &[u8]) -> [u8; N * 8] {
-//     // assumes all lengths are identical, not checked right now
+fn bitty_levenshtein_simd_by_1<const N: usize>(a: &[&[u8]; N], b: &[u8]) -> [u8; N] {
+    // assumes all lengths are identical, not checked right now
+    let (alen, blen) = (a[0].len(), b.len());
+    // myers-style algo
+    if blen == 0 {
+        return [alen as u8; N];
+    };
+    if alen == 0 {
+        return [blen as u8; N];
+    }
+    assert!(N == N); // something we'll change later
 
-//     let (ilim, jlim) = (a[0].len(), b.len());
-//     let mut prev: Vec<U8<N>> = (0..=J).map(|i| U8::<N>::splat(i as u8)).collect();
-//     let mut curr = vec![U8::<N>::splat(0); J + 1];
-//     let one = U8::<N>::splat(1);
+    let mut prev_hp = vec![U8::<N>::splat(255); blen];
+    let mut prev_hn = vec![U8::<N>::splat(0); blen];
+    //
+    let mut curr_hp = vec![U8::<N>::splat(255); blen];
+    let mut curr_hn = vec![U8::<N>::splat(0); blen];
 
-//     for i in 1..=I {
-//         curr[0] = U8::<N>::splat(i as u8);
+    for i in 0..alen {
+        // not needed actually
+        let mut curr_dp_j = U8::<N>::splat(0);
 
-//         let c_a = U8::<N>::from_array(std::array::from_fn(|s| a[s][i - 1] as u8));
+        let c_a = U8::<N>::from_array(std::array::from_fn(|s| a[s][i] as u8));
 
-//         for j in 1..=J {
-//             let mask = c_a.simd_eq(U8::<N>::splat(b[j - 1]));
+        for (j, cb) in b.iter().enumerate() {
+            let is_match = c_a
+                .simd_eq(U8::<N>::splat(*cb))
+                .select(U8::<N>::splat(255), U8::<N>::splat(0));
+            let curr_vp_j: U8<N>;
+            let curr_vn_j: U8<N>;
+            if j == 0 {
+                curr_vp_j = U8::<N>::splat(255);
+                curr_vn_j = U8::<N>::splat(0);
+            } else {
+                // curr_d[j - i] = prev_h[ j - 1 ] + curr_v [ j ]
+                // res := curr_dp[j - 1] - prev_hp[j - 1] + prev_hn[j - 1];
+                let curr_dp_j_m1 = curr_dp_j;
+                curr_vp_j = prev_hn[j - 1] | (curr_dp_j_m1 & !prev_hp[j - 1]); // res > 0
+                curr_vn_j = prev_hp[j - 1] & !curr_dp_j_m1; // res < 0
+            }
+            // curr_d[j], before we used previous variable
+            curr_dp_j = !(prev_hn[j] | curr_vn_j | is_match);
+            {
+                // curr_h[j] = curr_d[j] - curr_v[j]
+                // res := curr_dp[j] - curr_vp[j] + curr_vn[j];
+                curr_hp[j] = (curr_dp_j & !curr_vp_j) | curr_vn_j; // res > 0
+                curr_hn[j] = curr_vp_j & !curr_dp_j; // res < 0
+            }
+        }
+        if i + 1 < alen {
+            std::mem::swap(&mut prev_hn, &mut curr_hn);
+            std::mem::swap(&mut prev_hp, &mut curr_hp);
+        }
+    }
 
-//             curr[j] = (mask.select(prev[j - 1], prev[j - 1] + one))
-//                 .simd_min(prev[j] + one)
-//                 .simd_min(curr[j - 1] + one)
-//         }
-//         std::mem::swap(&mut prev, &mut curr);
-//     }
-//     return *prev[J].as_array();
-// }
+    let one = U8::<N>::splat(1);
+
+    for j in 0..blen {
+        curr_hp[j] = curr_hp[j] & one;
+        curr_hn[j] = curr_hn[j] & one;
+    }
+
+    let result: U8<N> =
+        U8::<N>::splat(alen as u8) + curr_hp.iter().sum::<U8<N>>() - curr_hn.iter().sum::<U8<N>>();
+    let res2: [u8; N] = *result.as_array();
+    res2
+}
 
 #[allow(non_snake_case)]
 fn levenshtein_simd_by_1<const N: usize>(a: &[&[u8]; N], b: &[u8]) -> [u8; N] {
@@ -269,6 +312,16 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const TEST_SEQS: [&'static [u8]; 100] = [
+        b"a", b"Z9", b"k3x", b"T", b"q7", b"mN2", b"r", b"8b", b"L0p", b"dx", b"Y", b"w4R", b"3",
+        b"tK", b"p9q", b"H2", b"s", b"Vx1", b"7", b"nB", b"c4", b"J", b"u8m", b"5t", b"g", b"R2d",
+        b"y", b"0", b"eL", b"K9", b"z3Q", b"b", b"M1", b"f8", b"X", b"h2k", b"6", b"dP", b"q",
+        b"9z", b"W4", b"l", b"C7r", b"2", b"vN", b"t", b"8Kx", b"G", b"m5", b"p", b"1aZ", b"r4",
+        b"S", b"y7", b"k", b"D3", b"0x", b"n", b"B8q", b"u", b"4", b"e2R", b"L", b"c9", b"Tm",
+        b"7pQ", b"a", b"Z", b"x3", b"H", b"j8L", b"2k", b"w", b"F5", b"9", b"sD", b"q1", b"U",
+        b"b7", b"6m", b"Y2", b"t", b"K", b"p4X", b"r", b"3d", b"V", b"g8", b"N1c", b"z", b"5R",
+        b"h", b"0Lk", b"M", b"y2", b"C", b"8t", b"f", b"Q7", b"d",
+    ];
 
     #[test]
     fn test_bitty_0_0() {
@@ -308,25 +361,27 @@ mod tests {
     }
 
     #[test]
-
     fn test_bitty_random_small() {
-        let data: Vec<&'static [u8]> = vec![
-            b"a", b"Z9", b"k3x", b"T", b"q7", b"mN2", b"r", b"8b", b"L0p", b"dx", b"Y", b"w4R",
-            b"3", b"tK", b"p9q", b"H2", b"s", b"Vx1", b"7", b"nB", b"c4", b"J", b"u8m", b"5t",
-            b"g", b"R2d", b"y", b"0", b"eL", b"K9", b"z3Q", b"b", b"M1", b"f8", b"X", b"h2k", b"6",
-            b"dP", b"q", b"9z", b"W4", b"l", b"C7r", b"2", b"vN", b"t", b"8Kx", b"G", b"m5", b"p",
-            b"1aZ", b"r4", b"S", b"y7", b"k", b"D3", b"0x", b"n", b"B8q", b"u", b"4", b"e2R", b"L",
-            b"c9", b"Tm", b"7pQ", b"a", b"Z", b"x3", b"H", b"j8L", b"2k", b"w", b"F5", b"9", b"sD",
-            b"q1", b"U", b"b7", b"6m", b"Y2", b"t", b"K", b"p4X", b"r", b"3d", b"V", b"g8", b"N1c",
-            b"z", b"5R", b"h", b"0Lk", b"M", b"y2", b"C", b"8t", b"f", b"Q7", b"d",
-        ];
-        for a in data.iter() {
-            for b in data.iter() {
+        for a in TEST_SEQS.iter() {
+            for b in TEST_SEQS.iter() {
                 let a_str = std::str::from_utf8(a).unwrap();
                 let b_str = std::str::from_utf8(b).unwrap();
                 let bitwise_result = bitty_levenshtein(a, b) as i32;
                 let reference = levenshtein(a_str, b_str) as i32;
                 assert_eq!(bitwise_result, reference);
+            }
+        }
+    }
+
+    #[test]
+    fn test_bitty_simd_1_to_n() {
+        for a in TEST_SEQS.iter() {
+            for b in TEST_SEQS.iter() {
+                let a_str = std::str::from_utf8(a).unwrap();
+                let b_str = std::str::from_utf8(b).unwrap();
+                let bitwise_result = bitty_levenshtein_simd_by_1(&[*a; 8], b)[0] as i32;
+                let reference = levenshtein(a_str, b_str) as i32;
+                assert_eq!(bitwise_result, reference, "{} {}", a_str, b_str);
             }
         }
     }
